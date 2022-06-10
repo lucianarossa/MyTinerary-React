5afbf307728c0532c4988622c5d35bfa468d14c9
luciana rossa <lucianarossa@gmail.com>
2022-06-10 15:25:20 -0300

    servidor

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express()
+
+const PORT = 4000
+
+app.set ('port', PORT)
+
+app.get ('/', (req, res) => {
+    res.send('SERVIDOR CREADO!')
+})
+
+app.listen(PORT, () => {
+    console.log('SERVIDOR CORRIENDO EN PUERTO: ' + app.get ('port'))
+})
