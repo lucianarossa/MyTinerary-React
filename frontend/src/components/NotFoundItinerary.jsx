3533473bbcc4ae16bd497e6e2fff73e4a073afba
luciana rossa <lucianarossa@gmail.com>
2022-06-24 01:54:41 -0300

    final completo

--- frontend/src/components/NotFoundItinerary.jsx
+++ frontend/src/components/NotFoundItinerary.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NotFoundItinenary() {
     return (
         <div className="notfound-container">
-            <h1 className="notf-title">Our insiders haven't shared itineraries for this city ...try another destiny!</h1>
+            <h1 className="notf-title">Sorry!! Our insiders have not shared itineraries for this city... You can choose another destination!</h1>
             <img className="notfounditi" src={process.env.PUBLIC_URL + "/assets/notfound2.png"} alt="notfound" />
         </div>
     )
