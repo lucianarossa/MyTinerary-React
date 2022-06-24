35bcf656b69f57b6f2a5f7b91cd3634f53f67c21
luciana rossa <lucianarossa@gmail.com>
2022-06-24 01:44:35 -0300

    retoqes finales css

--- frontend/src/components/Itinerary.jsx
+++ frontend/src/components/Itinerary.jsx
@@ -8,12 +8,13 @@ import { User } from "@nextui-org/react";
 function Itinerary({ data }) {
 
     return (
-        <Card className="iti-card" isHoverable variant="bordered" css={{mw: "80%", padding:"0",backgroundColor: "white", boxShadow: "0px 5px 8px rgba(0, 0, 0, 0.505)" }}>
-            {data.map(itinerary => (
+        <>
+        {data.map(itinerary => (
+        <Card className="iti-card" isHoverable variant="bordered" css={{mw: "80%", padding:"2",backgroundColor: "white", boxShadow: "0px 5px 8px rgba(0, 0, 0, 0.505)" }}>
                 <div key={itinerary._id} className="card-box">
                     <Card.Body css={{ w: "100%" }}>
                         <div>
-                            <Text className="iti-title" css={{textShadow: "5px 0px 45px rgba(4, 4, 4, 0.911)" }}>{itinerary.name}</Text>
+                            <Text className="iti-title" css={{textShadow: "5px 4px 30px rgba(4, 4, 4, 0.911)" }}>{itinerary.name}</Text>
                             <User
                                 src={process.env.PUBLIC_URL + `${itinerary.authorimage}`}
                                 name={itinerary.author}
@@ -22,7 +23,7 @@ function Itinerary({ data }) {
                                 zoomed
                                 squared
                             />
-                            <Text h6 css={{ w: "100%", paddingTop: "3rem", paddingBottom: "2rem" }}>"{itinerary.description}"</Text>
+                            <Text h6 css={{ w: "100%"}}>"{itinerary.description}"</Text>
                             <div className="price-duration">
                                 <Text className="price-dur">Price:  {itinerary.price}</Text>
                                 <Text className="price-dur">Duration:  {itinerary.duration} hs</Text>
@@ -39,10 +40,10 @@ function Itinerary({ data }) {
                             </div>
                         </div>
                     </Card.Body>
-                    <Grid.Container gap={2} css={{ w: "100%" }}>
+                    <Grid.Container gap={0} css={{ w: "100%" }}>
                         <Grid>
                             <Collapse.Group divider={false}>
-                                <Collapse title="MORE INFO" shadow css={{ backgroundColor: "#809090", boxShadow: "0px 5px 8px rgba(0, 0, 0, 0.505)", marginBottom: "1rem", }}>
+                                <Collapse title="MORE INFO" shadow css={{ backgroundColor: "#8090909f", boxShadow: "0px 5px 8px rgba(0, 0, 0, 0.505)", marginBottom: "0", }}>
                                     <div>
                                         <h3 className="funtitle">FUN ACTIVITIES COMING SOON !!!</h3>
                                     </div>
@@ -52,10 +53,11 @@ function Itinerary({ data }) {
                         </Grid>
                     </Grid.Container>
                 </div>
-            ))}
         </Card>
+         ))}
+          </>
     )
-
+   
 
 }
 
