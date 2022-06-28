61a42edd61256196363876683886afe5bb15863b
luciana rossa <lucianarossa@gmail.com>
2022-06-28 10:07:07 -0300

    controllers y modelo

---
+++ controllers/usersControllers.js
@@ -0,0 +1,121 @@
+const User = require('../models/users')
+
+const usersControllers = {
+    signUpUsers: async (req, res) => {
+        let {firstName, lastName, email, password, image, from} = req.body.userData
+        try{
+            const userExist = await User.findOne({email})
+            if(userExist) {
+               if (userExist.from.indexOf(from) !== -1){
+                res.json({
+                    success: false,
+                    from: "signup",
+                    message: "You have already Sign Up in this way please Sign In"
+                })
+                } else {
+                const hashedPassword = bcryptjs.hashSync(password, 10)
+                userExist.from.push(from)
+                userExist.password.push(hashedPassword)
+                res.json({
+                    success: true,
+                    from: "signup",
+                    message: "We have added" + from + "to your means of registration"
+                })
+            }
+        } else {
+            const hashedPassword = bcryptjs.hashSync(password, 10)
+            const newUser = await new User({
+                firstName,
+                lastName,
+                email,
+                password,
+                image,
+                from: [from],
+            })
+            if (from !== "form-signup") {
+                await newUser.save()
+                res.json({
+                    success: true,
+                    from: "signup",
+                    message: "Congratulations your user has been created with" + from
+                })
+            } else {
+                await newUser.save()
+                res.json({
+                    success: true,
+                    from: "signup",
+                    message: "We sent you an email to validate your registration, please check your email box" 
+                })
+            }
+         }
+    } catch (error) {
+        res.json({ success: false, message: "Ups! Something is wrong, try in a few minutes"})
+    }
+},
+
+
+
+
+    logInUser: async (req, res) => {
+        const {email, password, from} = req.body.loguedUser
+        try{
+            const userExist = await User.findOne({email})
+            const indexPass = userExist.from.indexOf(from)
+            if(!userExist){
+                res.json({success: false, message: "You haven't registered yet please Sign Up"})
+            } else {
+                if(from !== "form-signup"){
+                    let matchpassword = userExist.password.filter(pass => bcryptjs.compareSync(password, pass))
+                    if(matchpassword.length > 0) {
+                        const userData={
+                            id: userExist._id,
+                            firstName: userExist.firstName,
+                            email: userExist.email,
+                            from: from,
+                        }
+                    await userExist.save()
+                    res.json({
+                        success: true,
+                        from: from,
+                        response: {userData},
+                        message: "Welcome back" + userData.firstName,
+                    })
+
+                    } else {
+                        res.json({
+                            success: false,
+                            from: from,
+                            message: "you have not registered with" + from + "If you want to enter with this method you must sign in with" + from
+                        })
+                    }
+                } else {
+                    if(matchpassword.length > 0) {
+                        const userData = {
+                            id: userExist._id,
+                            firstName: userExist.firstName,
+                            email: userExist.email,
+                            from: from,
+                        }
+                        await userExist.save()
+                        res.json({
+                            success:true,
+                            from: from,
+                            response: { token, userData},
+                            message: "Welcome back" + userData.firstName,
+                        })
+                    } else {
+                        res.json({
+                            success: false,
+                            from: from,
+                            message: "username or password do not match",
+                        })
+                    }
+                }
+            }
+        } catch (error) {
+            res.json({success: false, message:"Ups! Something is wrong, try in a few minutes"})
+        }
+    },
+}
+
+module.exports = usersControllers
---
+++ models/user.js
@@ -0,0 +1,17 @@
+//mongoose permite crear el modelo
+const mongoose = require ("mongoose")
+
+//Defino el esquema (objeto de la colecc).
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    image: {type: String, required: true},
+    from: {type: Array}
+
+})
+
+const User = mongoose.model("users", userSchema) //crea en la colecc, el schema
+module.exports = User //exporto
